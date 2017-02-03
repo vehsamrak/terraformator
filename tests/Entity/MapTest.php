675fbc1ba1146e525261d114931c303c31810d81
Petr Karmashev <smonkl@bk.ru>
2017-02-03 13:56:29 +0300

    Unnecessary test removed from MapTest

--- tests/Entity/MapTest.php
+++ tests/Entity/MapTest.php
@@ -13,14 +13,6 @@ class MapTest extends \PHPUnit_Framework_TestCase
 {
 
     /** @test */
-    public function construct_noParameters_newMapCreated(): void
-    {
-        $map = new Map();
-
-        $this->assertInstanceOf(Map::class, $map);
-    }
-
-    /** @test */
     public function add_emptyMap_mapLocationAddedToMap()
     {
         $map = new Map();
