0a0ba888a476a1e5f37c49749a04894c680ff4d9
Petr Karmashev <smonkl@bk.ru>
2017-02-08 03:05:05 +0300

    Map::getPreviousLocations() method

--- src/Entity/Map.php
+++ src/Entity/Map.php
@@ -40,5 +40,33 @@ class Map extends ArrayCollection
 
         return $firstLocation;
     }
+
+    public function getPreviousLocations(int $x, int $y): PreviousLocationMap
+    {
+        // filtering north, north-west, north-east and west locations by X and Y
+        $previousLocations = $this->filter(
+            function (Location $location) use ($x, $y) {
+                $westLocationX = $x - 1;
+                $westLocationY = $y;
+                $northWestLocationX = $x - 1;
+                $northWestLocationY = $y - 1;
+                $northLocationX = $x;
+                $northLocationY = $y - 1;
+                $northEastLocationX = $x + 1;
+                $northEastLocationY = $y - 1;
+
+                $locationX = $location->getX();
+                $locationY = $location->getY();
+
+                return ($locationX === $westLocationX && $locationY === $westLocationY) ||
+                    ($locationX === $northWestLocationX && $locationY === $northWestLocationY) ||
+                    ($locationX === $northLocationX && $locationY === $northLocationY) ||
+                    ($locationX === $northEastLocationX && $locationY === $northEastLocationY);
+
+            }
+        );
+
+        return new PreviousLocationMap($previousLocations->getValues());
+    }
 }
 
